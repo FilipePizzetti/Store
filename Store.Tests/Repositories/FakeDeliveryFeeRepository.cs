63c37e2746c9139308565b0f79912230cd5d05ac
Filipe Pizzetti <pizzetti.filipe@gmail.com>
2025-01-28 20:10:47 -0300

    adicionado tests para queries

---
+++ Store.Tests/Repositories/FakeDeliveryFeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Store.Domain.Repositories;
+
+namespace Store.Tests.Repositories
+{
+    public class FakeDeliveryFeeRepository : IDeliveryFeeRepository
+    {
+        public decimal Get(string zipCode)
+        {
+            if (zipCode == "88802050")
+                return 10;
+            return 0;
+        }
+    }
+}
